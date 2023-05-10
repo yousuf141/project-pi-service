ebe0bc991a76aea6fa4fb99dd71e766888fc0cb9
yousuf141 <77011760+yousuf141@users.noreply.github.com>
2023-05-10 18:44:59 +0100

    create env template

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  WS_ENDPOINT: process.env.WS_ENDPOINT ?? "http://localhost",
+  WS_ENDPOINT: process.env.WS_ENDPOINT ?? "http://localhost:3000",
   LIGHT_PIN: process.env.LIGHT_PIN ?? 4,
   FAN_PIN: process.env.FAN_PIN ?? 6,
 };
